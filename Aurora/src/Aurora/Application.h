970cef8b2ea806289448ba438a5f2fb0a23d0b6f
gray_reaven <c3119426342@gmail.com>
2023-04-06 13:02:08 +0800

    【功能开发】          -添加OpenGL着色器 [Feature Development] -Add First Shader.

--- Aurora/src/Aurora/Application.h
+++ Aurora/src/Aurora/Application.h
@@ -9,6 +9,8 @@
 
 #include "Aurora/ImGui/ImGuiLayer.h"
 
+#include "Aurora/Renderer/Shader.h"
+
 namespace Aurora {
 
 	class AURORA_API Application
@@ -39,6 +41,7 @@ namespace Aurora {
 		LayerStack m_LayerStack;
 
 		unsigned int m_VertexArray, m_VertexBuffer, m_IndexBuffer;
+		std::unique_ptr<Shader> m_Shader;
 	private:
 		static Application* s_Instance;
 	};
