fb1b8d0889c8d2d51215cd5c90778ce392f67eae
gray_reaven <c3119426342@gmail.com>
2023-04-08 15:22:31 +0800

    【功能开发】 -创建基础的引用系统

--- Aurora/src/Platform/Windows/WindowsWindow.cpp
+++ Aurora/src/Platform/Windows/WindowsWindow.cpp
@@ -9,7 +9,7 @@
 
 namespace Aurora {
 
-	static bool s_GLFWInitialized = false;
+	static uint8_t s_GLFWWindowCount = 0;
 
 	static void GLFWErrorCallback(int error, const char* description)
 	{
@@ -39,17 +39,18 @@ namespace Aurora {
 
 		AUR_CORE_INFO("Creating window {0} {1} {2}", props.Title, props.Width, props.Height);
 
-		if (!s_GLFWInitialized)
+		if (s_GLFWWindowCount==0)
 		{
+			AUR_CORE_INFO("Initializing GLFW");
 			// TODO: glfwTerminate on system shutdown
 			int success = glfwInit();
 			AUR_CORE_ASSERT(success, "Could not initialize GLFW!");
 			glfwSetErrorCallback(GLFWErrorCallback);
 
-			s_GLFWInitialized = true;
 		}
 
 		m_Window = glfwCreateWindow((int)props.Width, (int)props.Height, m_Data.Title.c_str(), nullptr, nullptr);
+		++s_GLFWWindowCount;
 
 		m_Context = CreateScope<OpenGLContext>(m_Window);
 		m_Context->Init();
@@ -162,7 +163,11 @@ namespace Aurora {
 
 	void WindowsWindow::Shutdown()
 	{
-		glfwDestroyWindow(m_Window);
+		if (--s_GLFWWindowCount == 0)
+		{
+			AUR_CORE_INFO("Terminating GLFW");
+			glfwTerminate();
+		}
 	}
 
 	void WindowsWindow::OnUpdate()
